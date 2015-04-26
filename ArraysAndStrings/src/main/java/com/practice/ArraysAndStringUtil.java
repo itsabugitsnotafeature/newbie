0e33a41f7112821eb691f822a6bd8737fe6331d5
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2015-04-26 03:02:20 -0700

    Question : Given two strings, write method to check if FIRST string  is a permutation of SECOND string. Implements : isPermutationSecond
    Also tested in the utilTest java class.

--- ArraysAndStrings/src/main/java/com/practice/ArraysAndStringUtil.java
+++ ArraysAndStrings/src/main/java/com/practice/ArraysAndStringUtil.java
@@ -4,14 +4,33 @@ import java.util.Arrays;
 
 public class ArraysAndStringUtil {
 	
+	public static boolean isPermutationSecond ( String first, String second ) {
+		System.out.println("String#1 : " + first + "  String#2 : " + second);
+		
+		if (first.length() != second.length())
+			return false;
+		
+		int[] letters = new int[256];	// Assumption
+		char[] firstArray = first.toCharArray();
+		
+		for (char c : firstArray) {
+			letters[c]++;
+		}
+		
+		for (int i=0; i < second.length() ; i++ ) {
+			char c = second.charAt(i);
+			if ( --letters[c] < 0)
+				return false;
+		}
+		return true;
+	}
+	
+	
 	public static boolean isPermutation ( String first, String second ) {
-		System.out.println("Start1 : " + first);
-		System.out.println("Start2 : " + second);
+		System.out.println("String#1 : " + first + "  String#2 : " + second);
 		
 		if ( first.length() != second.length())
 			return false;
-		
-		
 //		System.out.println("1 : " + firstContent);
 //		System.out.println("1 : " + secondContent);
 		return false;
@@ -23,5 +42,6 @@ public class ArraysAndStringUtil {
 		Arrays.sort(content);
 		return new String(content);
 	}
+	
 	
 }
--- ArraysAndStrings/src/test/java/com/practice/ArraysAndStringUtilTest.java
+++ ArraysAndStrings/src/test/java/com/practice/ArraysAndStringUtilTest.java
@@ -6,11 +6,20 @@ import junit.framework.TestCase;
 
 public class ArraysAndStringUtilTest extends TestCase {
 	
+	/*
 	@Test
 	public void testIsPermutation() {
 		assertFalse(ArraysAndStringUtil.isPermutation("aabbcc","ccaabb"));
-		
-//		assertFalse(ArraysAndStringUtil.isPermutation("O, Draconian devil!","Oh, lame saint!"));
+		assertFalse(ArraysAndStringUtil.isPermutation("O, Draconian devil!","Oh, lame saint!"));
+	}
+	*/
+	
+	@Test
+	public void testIsPermutationSecond() {
+		assertTrue(ArraysAndStringUtil.isPermutationSecond("aabbcc","ccaabb"));
+		assertFalse(ArraysAndStringUtil.isPermutationSecond("o, draconian devil!","oh, lame saint!"));
+		assertTrue(ArraysAndStringUtil.isPermutationSecond("dormitory","dirtyroom"));
+		assertTrue(ArraysAndStringUtil.isPermutationSecond("aeaabccdddeeeee","dbeceaeceadadee"));
 	}
 
 }
